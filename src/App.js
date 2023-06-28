591d731bfe77931a70ee51c746bae4d46e56f66a
AdonayDereje <81191245+AdonayDereje@users.noreply.github.com>
2023-06-28 21:08:31 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TravelCard from './components/TravelCard'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="mainSection">
+        <TravelCard />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <div className="header">
+            <img src="/images/travel-journal-logo.png" className="header--logo" alt="World Logo"/>
+            <p className="header--title">my travel journal.</p>
+        </div>
+    )
+}
+
+export default Header;
