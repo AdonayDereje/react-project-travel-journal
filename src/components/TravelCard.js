49f12fc43f8089dbf371b6c6f8f432427a43edef
AdonayDereje <81191245+AdonayDereje@users.noreply.github.com>
2023-06-29 13:48:47 +0300

    removed alt in TravelCard.js : img

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -4,9 +4,9 @@ function TravelCard (props){
     return(
         <>
             <div className="travelCard">
-                <img className="travelCard--img" src={props.item.img} alt="Image of Mt Fuji"/>
+                <img className="travelCard--img" src={props.item.img}/>
                 <div className="travelCard--content">
-                    <img className="travelCard--pinLogo" src="/images/PinLogo.svg" alt="Location Pin" />
+                    <img className="travelCard--pinLogo" src="/images/PinLogo.svg"/>
                     <div className="travelCard--location">{props.item.location}</div>
                     <a className = "travelCard--googleMapLink" href = {props.item.googleMapLink} target='blank'>View on Google Maps</a>
                     <div className="travelCard--title">{props.item.title}</div>
